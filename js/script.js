e313fb916beefd9661ebbd09e4e1c8a9f9bdf090
James Crosslin <jamesmscrosslin@gmail.com>
2020-12-24 21:23:22 -0800

    added random background color generation

--- js/script.js
+++ js/script.js
@@ -1,30 +1,12 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-
-// For assistance:
-// Check the "Project Resources" section of the project instructions
-// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/***
- * `quotes` array
- ***/
-
-// {
-//   quote: ,
-//   source: ,
-//   citation: ,
-//   year: ,
-// },
-
-//This is an array containing quote objects
-
+/*
+ *This is an array containing quote objects
+ */
 const quotes = [
   {
     quote:
       "I would be ashamed to admit that I had risen from the ranks. When I rise it will be with the ranks, and not from the ranks.",
     source: "Eugene V. Debs",
+    profession: "American politician",
     citation: "Canton, Ohio Anti-War Speech",
     year: 1918,
   },
@@ -32,58 +14,86 @@ const quotes = [
     quote:
       "Let us realize the arc of the moral universe is long but it bends toward justice.",
     source: "Martin Luther King, Jr.",
+    profession: "Minister",
     citation: "Where do we go from Here: Chaos or Community?",
     year: 1967,
   },
   {
     quote: "I acknowledge no master in human form.",
     source: "John Brown",
+    profession: "Revolutionary",
     year: 1859,
   },
   {
     quote:
       "I would unite with anybody to do right; and with nobody to do wrong.",
     source: "Frederick Douglass",
+    profession: "Scholar and activist",
     citation: "Lecture, The Anti-Slavery Movement",
     year: 1855,
   },
   {
     quote: "People say nothing is impossible, but I do nothing every day.",
     source: "A.A. Milne",
+    profession: "Author",
   },
   {
     quote: `We are never quits with those who oblige us," was Dantes' reply; "for when we do not owe them money, we owe them <em>gratitude</em>.`,
     source: "Alexandre Dumas",
+    profession: "Author",
     citation: "The Count of Monte Cristo",
     year: 1845,
   },
 ];
 
+/*
+ * This function creates a random number within a range
+ * with the minimum and maximum values as parameters
+ */
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 /***
- * `getRandomQuote` function
+ * This function retrieves a random number and uses
+ * that number as an index for the quotes array
  ***/
 function getRandomQuote() {
-  const randomNum = Math.floor(Math.random() * quotes.length);
-  return quotes[randomNum];
+  const randomIndex = getRandomNumber(0, quotes.length);
+  return quotes[randomIndex];
 }
 
 /***
- * `printQuote` function
+ * The printQuote function retrieves a random
+ * quote and adds that quote to the DOM
  ***/
-
 function printQuote() {
   const randomQuote = getRandomQuote();
-  const htmlString = `
+  let quoteString = `
   <p class="quote">${randomQuote.quote}</p>
-  <p class="source">${randomQuote.source}</p>
-  `;
+  <p class="source">`;
+
+  if (randomQuote.profession) quoteString += `<span class='profession'>${randomQuote.profession}</span>`;
+
+  quoteString += randomQuote.source;
+
+  if (randomQuote.citation)quoteString += `<span class="citation">${randomQuote.citation}</span>`;
+  if (randomQuote.year) quoteString += `<span class="year">${randomQuote.year}`;
+
+  quoteString += `</p>`;
+
+  document.querySelector("body").style.backgroundColor = `rgb(
+    ${getRandomNumber(0, 255)},${getRandomNumber(0, 255)},${getRandomNumber(0, 255)}
+    )`;
+
+  return (document.getElementById("quote-box").innerHTML = quoteString);
 }
 
 /***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
+ * This event listener listens for a button click
+ * and calls the printQuote function
  ***/
-
 document
   .getElementById("load-quote")
   .addEventListener("click", printQuote, false);
